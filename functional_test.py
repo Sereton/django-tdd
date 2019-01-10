a8a02a0a1b4ae12fa647b7b8cb01e31889b6c8fe
Jay H <astrolillo@gmail.com>
2019-01-10 00:01:26 -0600

    Basic View return title

--- functional_test.py
+++ functional_test.py
@@ -10,8 +10,8 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         
-    def tearDown(self):
-        self.browser.quit()
+    #def tearDown(self):
+    #    self.browser.quit()
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
