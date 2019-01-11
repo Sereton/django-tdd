882f2ee6a7376ecb1d316ce350d68db1e704103a
Jay H <astrolillo@gmail.com>
2019-01-10 20:34:03 -0600

    Test checking the input of a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
+
 
 
 
@@ -10,13 +13,34 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         
-    #def tearDown(self):
-    #    self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
         
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         
         self.assertIn('To-Do', self.browser.title)
+        
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        inputbox = self.browser.find_element_by-id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), "Enter a to-do iteam"
+        )
+        
+        inputbox.send_keys('Buy peacock feathers')
+        
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        
+        self.assertTrue(
+            any(row.text== '1: Buy peacock feathers' for row in rows)
+        )
+        
         self.fail('Finish da test!!')
         
         
